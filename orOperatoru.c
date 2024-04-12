dc964586a436cba52b9145f09645755b07758fbf
berkaypzar <124529740+berkaypzar@users.noreply.github.com>
2024-04-12 16:33:53 +0300

    Create orOperatoru.c

---
+++ orOperatoru.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+#include <stdbool.h>
+int main(){
+
+double temp =25;
+
+  if(temp>=0 || temp<=30){
+
+    printf("Hava iyi durumda");
+}
+  else{
+printf("Hava kötü");
+    }
+  return 0;
+}
