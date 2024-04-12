94f8ad50ad6a5b5b5e8f0d0fcd803d1c0b32b52e
berkaypzar <124529740+berkaypzar@users.noreply.github.com>
2024-04-12 16:28:58 +0300

    Create andOperatoru.c

---
+++ andOperatoru.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+#include <stdbool.h>
+int main(){
+
+double temp =25;
+bool sunny=true;
+  
+  if(temp>=0 && temp<=30 && sunny){
+
+    printf("Hava iyi durumda");
+}
+  else{
+printf("Hava kötü");
+    }
+  return 0;
+}
