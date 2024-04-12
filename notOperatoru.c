18af2692f133f23370b4ccddd9394b697aa5bc17
berkaypzar <124529740+berkaypzar@users.noreply.github.com>
2024-04-12 16:44:44 +0300

    Create notOperatoru.c

---
+++ notOperatoru.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+#include <stdbool.h>
+int main(){
+  
+bool sunny= true;
+  if(!sunny){
+
+    printf("Hava bulutlu");
+}
+  else{
+printf("Hava güneşli");
+    }
+  return 0;}
