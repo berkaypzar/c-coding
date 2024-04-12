a83295bfc4bf0fbed085e28fad0fd63919982072
berkaypzar <124529740+berkaypzar@users.noreply.github.com>
2024-04-12 16:55:37 +0300

    Create fonksiyonOlusturma.c

---
+++ fonksiyonOlusturma.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+
+void bp(){
+
+printf("Merhaba benim adım Berkay\n");
+printf("21 yaşındayım\n");
+printf("TED Üniversitesi'nde okuyorum\n");
+printf("Bilgisayar Mühendisliği bölümünde okuyorum\n");  
+printf("\n");}
+
+
+int main(){
+bp();
+  bp();
+  bp();
+
+  
+  return 0;}
