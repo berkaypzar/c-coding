cd7dd4a25f76077eaed818dbad9963ed18602682
berkaypzar <124529740+berkaypzar@users.noreply.github.com>
2024-04-21 14:27:14 +0300

    Create aritmetikOperatorler.c

---
+++ aritmetikOperatorler.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+
+//toplama "+"
+// çıkarma "-"
+  //çarpma "*"
+  //bölme "/"
+  // mod alma " %"
+  // üs alma pow(x,y)
+  // artırma ++
+  //azaltma --
+
+  int x=6;
+  int y=4;
+  int z=x+y;
+printf("Sonuç = %d",z ); 
+
+ return 0;
+}
