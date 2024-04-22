0942cad3e1e07c36f5ff5928e5a96a8d89a4ba1d
berkaypzar <124529740+berkaypzar@users.noreply.github.com>
2024-04-22 13:22:57 +0300

    Create continueAndBreaak.c

---
+++ continueAndBreaak.c
@@ -0,0 +1,32 @@
+/*#include <stdio.h>
+
+int main(){
+
+// contiune = kodun geri kalanını atlar ve döngünün sonraki tekrarına geçer
+// break = kodun geri kalanını atlar ve döngüden çıkar
+
+  /*for(int i = 1; i<=20; i++){
+
+    if(i==10){
+      continue;
+    }
+    printf("%d\n",i);
+  }
+*/
+  
+    /*for(int i = 1; i<=20; i++){
+
+      if(i==10){
+        break;
+      }
+      printf("%d\n",i);
+    }
+
+
+
+
+
+
+return 0;
+  
+} */
