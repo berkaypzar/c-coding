6f9822533db834a1c62c0128ae97cbf5faa6e5ac
berkaypzar <124529740+berkaypzar@users.noreply.github.com>
2024-04-22 18:26:40 +0300

    Create ifElse.c

---
+++ ifElse.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+
+
+int age;
+
+  printf("Yaşınızı giriniz:");
+  scanf("%d",&age);
+
+
+  if(age>=18){
+
+  printf("Kursa kaydınız başarılı.");
+  }
+else{
+
+  printf("Kursa kayıt olamadınız. Yaşınız tutmuyor.");
+}
+
+
+return 0;
+
+
+}  
