9dafb115dd24d79f393c7491ae7b9ee89d1d1314
berkaypzar <124529740+berkaypzar@users.noreply.github.com>
2024-04-22 14:27:58 +0300

    Create arrays.c

---
+++ arrays.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+
+//int ages[] = {18,25,20,16};
+int ages[4];
+ages[0] = 18;
+ages[1] = 25;
+ages[2] = 20;
+ages[3] = 16;  
+  printf("%d",ages[0]);
+
+
+
+
+
+  return 0;
+}
