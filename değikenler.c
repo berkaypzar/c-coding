1c882ff1f695637913f1510d58c18c55c16153ce
berkaypzar <124529740+berkaypzar@users.noreply.github.com>
2024-04-22 18:22:38 +0300

    Create değikenler.c

---
+++ değikenler.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+
+int x;
+  x=123;
+int y=321;
+
+int level = 87;
+double xp = 12.05;
+char server = 'A';
+char nick[] = "BP";
+
+
+  printf("Merhaba %s\n",nick);
+  printf("Şu an %d levele sahipsin\n",level);
+  printf("XP durumun %%%lf\n",xp);
+  printf("Sunucu adı %c",server);
+
+
+
+  return 0;
+}
+
