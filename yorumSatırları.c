f04ed31b4ac9bdd4871b7f018a7054c7675e10b0
berkaypzar <124529740+berkaypzar@users.noreply.github.com>
2024-04-22 18:33:08 +0300

    Create yorumSatırları.c

---
+++ yorumSatırları.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/*int main(){
+// bu bir yorum satırıdır.//
+  
+  /*bdfvsbdkgbkbgkbskghksdghdslghkdlshgksdhgkldh
+sjgbdkgbksdbgbsdkg
+    bgkdbsgdkjsgkjdbgk
+    kngklsndgnlksdnglkns  bu bir yorum satırlarıdır.
+    */
+
+  // \n yeni satır//
+  // \t geniş  boşluk//
+  // \' tek tırnağı metinde gösterir//
+  // \"" çift tırnağı metinde gösterir//
+  
+/*printf("hadi agalar\n");
+
+  printf("1\t2\t3\n4\t5\t6\n");
+  printf("\"barbeküde et yanına salata\"- ünlü (sanatçı) çakal\n");
+  printf("\\barbeküde et yanına salata\\ - ünlü sanatçı çakal \n");
+  printf("\'barbeküde et yanına salata\' - ünlü sanatçı çakal "); 
+
+
+  return 0; 
+}   
