2ef135a744840b55cc23eccd7c1e1fd35d833986
berkaypzar <124529740+berkaypzar@users.noreply.github.com>
2024-04-22 18:28:19 +0300

    Create matFonksiyonları.c

---
+++ matFonksiyonları.c
@@ -0,0 +1,30 @@
+/*#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+double A = sqrt(25);
+double B = pow(2,3);
+int C = round(3.14); 
+int D = ceil(3.01);
+int E = floor(3.99);
+double F = fabs(-100);
+double G = log(3);
+double H = sin(15);
+double I = cos(15);
+double J = tan(15);
+
+  printf("%lf\n",A);
+  printf("%lf\n",B);
+  printf("%d\n",C);
+printf("%d\n",D);
+  printf("%d\n",E);
+  printf("%lf\n",F);
+  printf("%lf\n",G);
+  printf("%lf\n",H);
+  printf("%lf\n",I);
+  printf("%lf\n",J);
+
+
+  return 0;
+}  */
